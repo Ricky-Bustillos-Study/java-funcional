a2b33fba80466d88071ed278f5c8af0856fd5372
Ricky Bustillos <henrique.ssbl@hotmail.com>
2021-08-18 16:01:39 -0300

    Functions

---
+++ src/main/java/one/digitalinnovation/Functions.java
@@ -0,0 +1,18 @@
+package one.digitalinnovation;
+
+import java.util.function.Function;
+
+public class Functions {
+
+    public static void main(String[] args) {
+
+        Function<String, String> returnReversedName = text -> new StringBuilder(text).reverse().toString();
+        System.out.println(returnReversedName.apply("Ricky"));
+
+        Function<String, Integer> convertStringToInteger = Integer::valueOf;
+        System.out.println(convertStringToInteger.apply("2")); // 2
+
+        Function<String, Integer> convertStringToIntegerAndMultiply = string -> Integer.valueOf(string) * 5;
+        System.out.println(convertStringToIntegerAndMultiply.apply("2")); // 10
+    }
+}
