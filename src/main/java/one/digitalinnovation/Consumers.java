daa7142cb96ea3bc1257b06cc4493bcb6469bbe1
Ricky Bustillos <henrique.ssbl@hotmail.com>
2021-08-18 15:49:52 -0300

    Consumer

---
+++ src/main/java/one/digitalinnovation/Consumers.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation;
+
+import java.util.function.Consumer;
+
+public class Consumers {
+
+    public static void main(String[] args) {
+
+        // Consumer<String> printPhrase = phrase -> System.out.println(phrase);
+        Consumer<String> printPhrase = System.out::println;
+        printPhrase.accept("Hello world!");
+    }
+}
