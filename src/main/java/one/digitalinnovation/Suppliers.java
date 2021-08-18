956e031f30e49b23a6584785f661f7f99079f626
Ricky Bustillos <henrique.ssbl@hotmail.com>
2021-08-18 16:17:45 -0300

    Suppliers

---
+++ src/main/java/one/digitalinnovation/Suppliers.java
@@ -0,0 +1,30 @@
+package one.digitalinnovation;
+
+import java.util.function.Supplier;
+
+public class Suppliers {
+
+    public static void main(String[] args) {
+
+        // Supplier<Person> personInstance = () -> new Person();
+        Supplier<Person> personInstance = Person::new;
+
+        System.out.println(personInstance.get());
+    }
+}
+
+class Person {
+
+    private String name;
+    private Integer age;
+
+    public Person() {
+        name = "Ricky";
+        age = 21;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("name: %s, age: %d", name, age);
+    }
+}
